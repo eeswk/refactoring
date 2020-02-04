c7ecc302afb7a96d3fbeb6c806e88189ec3b016e
themorning <themorning84@gmail.com>
2020-02-05 00:06:20 +0900

    기본할인요금 할인정책 추가

---
+++ src/main/java/com/swan/obj/call/third/RateDiscountableRegularPhone.java
@@ -0,0 +1,17 @@
+package com.swan.obj.call.third;
+
+import java.time.Duration;
+
+public class RateDiscountableRegularPhone extends RegularPhone {
+    private Money discountAmount;
+
+    public RateDiscountableRegularPhone(Money amount, Duration seconds, Money discountAmount) {
+        super(amount, seconds);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}
